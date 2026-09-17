caedf88b3de0f93840afe772f871afeb204733b8
Владислав Иванов <vlado.ivanov1990@gmail.com>
2026-09-17 14:29:05 +0300

    vault backup: 2026-09-17 14:29:04

---
+++ Задачи/SimpleVariableIntroduction/SimpleVariableIntroduction/Program.cs
@@ -0,0 +1,8 @@
+﻿/*
+Create a simple C# program that declares 
+an integer variable, assigns it the value 10, 
+and prints the value to the console.
+*/
+
+int number = 10;
+Console.WriteLine(number);
---
+++ Код/DefiningvariablesInitializationAndAssignment/DefiningvariablesInitializationAndAssignment/Program.cs
@@ -0,0 +1,10 @@
+﻿string myName = "Vladislav";
+
+string petsName;
+petsName = "Daisy";
+Console.WriteLine(petsName);
+
+petsName = "Zara";
+Console.WriteLine(petsName);
+
+Console.ReadKey();
