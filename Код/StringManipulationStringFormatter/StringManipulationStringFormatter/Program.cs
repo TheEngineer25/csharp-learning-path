674d9be2fce213d1cb3a06460286df431d3bdfb6
Владислав Иванов <vlado.ivanov1990@gmail.com>
2026-09-21 15:27:31 +0300

    vault backup: 2026-09-21 15:27:31

---
+++ Код/StringManipulationStringFormatter/StringManipulationStringFormatter/Program.cs
@@ -0,0 +1,12 @@
+﻿
+int num = 10;
+double price = 19.95;
+string name = "Vladi";
+
+// interpolation
+Console.WriteLine($"The number is {num}");
+
+//COncatenation
+Console.WriteLine("The number is " + num);
+
+Console.ReadKey();
