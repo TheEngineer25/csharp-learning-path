a385c06eb720c724a3213e59d88273a3868856f2
Владислав Иванов <vlado.ivanov1990@gmail.com>
2026-09-21 12:07:44 +0300

    vault backup: 2026-09-21 12:07:44

--- Код/OperatorsOrderOfEvaltuationMathBasics/OperatorsOrderOfEvaltuationMathBasics/Program.cs
+++ Код/OperatorsOrderOfEvaltuationMathBasics/OperatorsOrderOfEvaltuationMathBasics/Program.cs
@@ -1,8 +1,8 @@
 ﻿int num1 = 5;
 int num2 = 13;
 
-Console.WriteLine("Addition num1 + num2 = " + num1 + num2);
-Console.WriteLine("Subtraction num1 - num2 = " + num1 - num2);
+Console.WriteLine("Addition num1 + num2 = " + (num1 + num2));
+Console.WriteLine("Subtraction num1 - num2 = " + (num1 - num2));
 Console.WriteLine("Multiplication num1 * num2 = " + num1 * num2);
 Console.WriteLine("Division num1 / num2 = " + num1 / num2);
 
