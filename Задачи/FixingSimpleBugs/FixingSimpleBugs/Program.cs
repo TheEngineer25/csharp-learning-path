f38f6c5a63619530f0177d0ac8bf23aa8e208966
Владислав Иванов <vlado.ivanov1990@gmail.com>
2026-09-21 15:07:09 +0300

    vault backup: 2026-09-21 15:07:09

---
+++ Задачи/FixingSimpleBugs/FixingSimpleBugs/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ 
+ Create a C# program that correctly calculates and displays the average of three numbers. The provided code has several bugs that prevent it from compiling and running correctly.
+
+Alert!
+
+The result of execution for the default string should be:
+
+"The average is: 20"
+ 
+ 
+ */
+
+int num1 = 10;
+int num2 = 20;
+int num3 = 30;
+int average = (num1 + num2 + num3) / 3;
+Console.WriteLine("The average is: " + average);
+
+
+
+
+
+
+Console.ReadKey();
---
+++ Лекции/25 - Тип данни char и  UTF-16 Smiley.md
@@ -0,0 +1,33 @@
+
+
+---
+type: csharp-note
+language: C#
+created: 2026-09-21
+tags:
+  - csharp
+---
+
+# Тип данни char и  UTF-16 Smiley
+
+## 📌 Какво ще се прави?
+
+
+
+## 🧠 Бележки
+
+
+
+## 💻 Синтаксис
+
+```csharp
+
+🧪 Пример
+
+[!important] Запомни
+
+[!warning] Честа грешка
+
+[!tip] Съвет
+
+🔗 Свързано
