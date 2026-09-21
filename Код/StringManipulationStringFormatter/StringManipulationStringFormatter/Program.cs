71b9dfa42057c124a1dc36d4cf04ebdf85a178a4
Владислав Иванов <vlado.ivanov1990@gmail.com>
2026-09-21 15:37:44 +0300

    vault backup: 2026-09-21 15:37:44

--- Код/StringManipulationStringFormatter/StringManipulationStringFormatter/Program.cs
+++ Код/StringManipulationStringFormatter/StringManipulationStringFormatter/Program.cs
@@ -6,7 +6,12 @@ string name = "Vladi";
 // interpolation
 Console.WriteLine($"The number is {num}");
 
-//COncatenation
+//Concatenation
 Console.WriteLine("The number is " + num);
+
+// string formatting
+Console.WriteLine("The number is {0}, and the price is {1}, and the name is {2}", num, price, name);
+
+
 
 Console.ReadKey();
