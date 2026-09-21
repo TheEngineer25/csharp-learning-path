60a594fa0ee5ad9ab0b80b82a1d489867c34cf90
Владислав Иванов <vlado.ivanov1990@gmail.com>
2026-09-21 09:45:05 +0300

    vault backup: 2026-09-21 09:45:05

--- Проекти/AdditionCalculator/AdditionCalculator/Program.cs
+++ Проекти/AdditionCalculator/AdditionCalculator/Program.cs
@@ -14,6 +14,8 @@ myNumber2 = int.Parse(userInput);
 
 int sum = myNumber + myNumber2;
 
-Console.WriteLine("The sum of " + myNumber + " and " + myNumber2 + " is " + sum);
+//Console.WriteLine("The sum of " + myNumber + " and " + myNumber2 + " is " + sum);
+
+Console.WriteLine($"The sum of {myNumber} and {myNumber2} is {sum}");
 
 Console.ReadKey();
