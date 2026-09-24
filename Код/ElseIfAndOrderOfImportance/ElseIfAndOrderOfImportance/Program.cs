29fddfca64c1c246fddef0f5c7627359c777d8c0
Владислав Иванов <vlado.ivanov1990@gmail.com>
2026-09-24 14:45:51 +0300

    vault backup: 2026-09-24 14:45:51

--- Код/ElseIfAndOrderOfImportance/ElseIfAndOrderOfImportance/Program.cs
+++ Код/ElseIfAndOrderOfImportance/ElseIfAndOrderOfImportance/Program.cs
@@ -1,9 +1,9 @@
-﻿int age = 16;
+﻿int age = int.Parse(Console.ReadLine());
 bool isWithParents = false;
 
 if (age >= 13 && isWithParents)
 {
-    Console.WriteLine("Go party in the club!");
+    Console.WriteLine("Go party in the club with your parents!");
 } else if (age > 18)
 {
     Console.WriteLine("Go party in the club!");
