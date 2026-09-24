30083b35a6fc3c8c1862a5d1becada3bc46cd35b
Владислав Иванов <vlado.ivanov1990@gmail.com>
2026-09-24 14:09:48 +0300

    vault backup: 2026-09-24 14:09:48

---
+++ Код/NestedIfStatements/NestedIfStatements/Program.cs
@@ -0,0 +1,26 @@
+﻿int num1 = 0;
+int num2 = 0;
+
+bool isEqual = num1 == num2;
+bool isNotEqual = num1 != num2;
+
+Console.WriteLine("Please enter a whole number:");
+
+if (num1 == int.Parse(Console.ReadLine()))
+{
+    Console.WriteLine("Numbers are equal!");
+
+    int age = 0;
+    if( age >= 18)
+    {
+        Console.WriteLine("Please enter your " +
+            "adress, so you can have the prize");
+
+        string address = Console.ReadLine();
+    }
+
+}
+else
+{
+    Console.WriteLine("Numbers are not equal!");
+}
