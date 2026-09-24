a4f38a756bcf611058c81ae4d4c021f2bf111495
Владислав Иванов <vlado.ivanov1990@gmail.com>
2026-09-24 10:26:41 +0300

    vault backup: 2026-09-24 10:26:41

---
+++ Код/ElseKeyword/ElseKeyword/Program.cs
@@ -0,0 +1,13 @@
+﻿int age = 18;
+
+if (age >= 18)
+{
+    Console.WriteLine("Go party in the club!");
+} else
+{
+    Console.WriteLine("Go party in school!");
+}
+
+
+
+Console.ReadKey();
--- Код/RelationalOperators/RelationalOperators/Program.cs
+++ Код/RelationalOperators/RelationalOperators/Program.cs
@@ -2,9 +2,9 @@
 int num2 = 6;
 
 bool isHigher = num1 > num2;
-int age = 7;
+int age = 18;
 
-if(age > 18)
+if(age >= 18)
 {
     Console.WriteLine("Go party in the club!");
 }
