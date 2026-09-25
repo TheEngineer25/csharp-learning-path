3dda323821fed7df4873879ed6b6351f8bf0e687
Владислав Иванов <vlado.ivanov1990@gmail.com>
2026-09-25 14:57:15 +0300

    vault backup: 2026-09-25 14:57:15

---
+++ Код/Switch/Switch/Program.cs
@@ -0,0 +1,30 @@
+﻿int month = 5;
+string monthName;
+
+if (month == 1)
+{
+    monthName = "January";
+}
+else if (month == 2)
+{
+    monthName = "February";
+}
+else if (month == 3)
+{
+    monthName = "March";
+}
+else
+{
+    monthName = "Unknown";
+}
+
+switch (month)
+{
+    case 1:
+        monthName = "January";
+        break;
+}
+
+
+
+Console.ReadKey();
